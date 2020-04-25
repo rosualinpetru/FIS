bf6640f26b2a5dd1878b16c2a9fc1bee4037b682
Rosu Alin-Petru <rosualinpetru@gmail.com>
2020-04-25 08:49:16 +0300

    First Commit! Hello World Program!

---
+++ src/main/java/ro/go/redhomeserver/tom/ToMApplication.java
@@ -0,0 +1,13 @@
+package ro.go.redhomeserver.tom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ToMApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ToMApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ro/go/redhomeserver/tom/ToMApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.go.redhomeserver.tom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ToMApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
