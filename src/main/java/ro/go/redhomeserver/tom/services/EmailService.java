dbb5c21a21436af80dc59f538c3d3f918343d974
Rosu Alin-Petru <rosualinpetru@gmail.com>
2020-04-28 16:58:09 +0300

    Implemented Send HTML Service

---
+++ src/main/java/ro/go/redhomeserver/tom/services/EmailService.java
@@ -0,0 +1,35 @@
+package ro.go.redhomeserver.tom.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.mail.MailException;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
+import org.springframework.mail.javamail.MimeMessagePreparator;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+    private final EmailContentBuilderService emailContentBuilderService;
+
+    @Autowired
+    public EmailService(JavaMailSender mailSender, EmailContentBuilderService emailContentBuilderService){
+        this.mailSender = mailSender;
+        this.emailContentBuilderService = emailContentBuilderService;
+    }
+
+    @Async
+    public void prepareAndSend(String recipient, String message) throws MailException {
+        MimeMessagePreparator messagePreparator = mimeMessage -> {
+            MimeMessageHelper messageHelper = new MimeMessageHelper(mimeMessage);
+            messageHelper.setFrom("tomapplication.dia@gmail.com");
+            messageHelper.setTo(recipient);
+            String content = emailContentBuilderService.build(message);
+            messageHelper.setText(content, true);
+        };
+        mailSender.send(messagePreparator);
+    }
+
+}
