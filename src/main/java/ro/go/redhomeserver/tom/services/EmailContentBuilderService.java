58bf3e206b00a5c51121f50ad908917f5752697f
Rosu Alin-Petru <rosualinpetru@gmail.com>
2020-04-28 18:11:37 +0300

    Added Email Dependency! Added Content Builder (Converts template to HTML email body).

---
+++ src/main/java/ro/go/redhomeserver/tom/services/EmailContentBuilderService.java
@@ -0,0 +1,24 @@
+package ro.go.redhomeserver.tom.services;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+
+@Service
+public class EmailContentBuilderService {
+
+    private final TemplateEngine templateEngine;
+
+    @Autowired
+    public EmailContentBuilderService(TemplateEngine templateEngine) {
+        this.templateEngine = templateEngine;
+    }
+
+    public String build(String message) {
+        Context context = new Context();
+        context.setVariable("message", message);
+        return templateEngine.process("emailTemplate", context);
+    }
+
+}
