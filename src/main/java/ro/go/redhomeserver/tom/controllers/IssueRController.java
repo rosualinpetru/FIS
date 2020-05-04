bee114b321965518fb897caae8756638a38442c5
Denisa-Rebeca Rosu <rosu_rebeca_denisa@yahoo.com>
2020-05-04 13:45:00 +0300

    Resolved redirect errors from controllers. Implemented report issue page. TODO: implement post for report issue

---
+++ src/main/java/ro/go/redhomeserver/tom/controllers/IssueRController.java
@@ -0,0 +1,49 @@
+package ro.go.redhomeserver.tom.controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import ro.go.redhomeserver.tom.exceptions.*;
+import ro.go.redhomeserver.tom.models.Account;
+import ro.go.redhomeserver.tom.services.AuthService;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.Map;
+
+
+@Controller
+
+public class IssueRController {
+
+    //@Autowired
+
+    @GetMapping("/reportIssue")
+    public ModelAndView reportIssue(HttpServletRequest request, RedirectAttributes ra) {
+        ModelAndView mv = new ModelAndView("reportIssue");
+        Account acc = (Account) request.getSession().getAttribute("active");
+        if(acc == null) {
+            mv = new ModelAndView("redirect:/auth");
+            ra.addFlashAttribute("upperNotification", "Please log in again (Session expired)!");
+            return mv;
+        }
+        mv.addObject("myID",acc.getId());
+        return mv;
+    }
+
+    @PostMapping("/reportIssue")
+
+    public String reportIssue(HttpServletRequest request) {
+        Account acc = (Account) request.getSession().getAttribute("active");
+        if (acc != null) {
+            //acc.
+            return "reportIssue";
+        } else
+            return "redirect:/auth";
+    }
+
+}
