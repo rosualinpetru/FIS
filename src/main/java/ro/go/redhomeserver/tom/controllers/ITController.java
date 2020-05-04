686a82ce2980bce6c8df28d8b20fd1b8e55accf2
Denisa-Rebeca Rosu <rosu_rebeca_denisa@yahoo.com>
2020-05-04 14:34:10 +0300

    Merged IssueReqController and IssueReqService into ITController and ITService

--- src/main/java/ro/go/redhomeserver/tom/controllers/ITController.java
+++ src/main/java/ro/go/redhomeserver/tom/controllers/ITController.java
@@ -4,13 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
-import ro.go.redhomeserver.tom.models.Department;
-import ro.go.redhomeserver.tom.models.Employee;
+import ro.go.redhomeserver.tom.models.Account;
 import ro.go.redhomeserver.tom.services.ITService;
+
+import javax.servlet.http.HttpServletRequest;
 import javax.transaction.SystemException;
-import java.security.NoSuchAlgorithmException;
-import java.util.Date;
+import java.util.Map;
 
 
 @Controller
@@ -19,6 +22,38 @@ public class ITController {
 
     @Autowired
     private ITService itService;
+
+
+
+    @GetMapping("/reportIssue")
+    public ModelAndView reportIssue(HttpServletRequest request, RedirectAttributes ra) {
+        ModelAndView mv = new ModelAndView("reportIssue");
+        Account acc = (Account) request.getSession().getAttribute("active");
+        if (acc == null) {
+            mv = new ModelAndView("redirect:/auth");
+            ra.addFlashAttribute("upperNotification", "Please log in again (Session expired)!");
+            return mv;
+        }
+        mv.addObject("myID", acc.getId());
+        return mv;
+    }
+
+    @PostMapping("/reportIssue")
+    public ModelAndView reportIssue(@RequestParam Map<String, String> params, RedirectAttributes ra, HttpServletRequest request) {
+        ModelAndView mv = new ModelAndView("redirect:/");
+        Account acc = (Account) request.getSession().getAttribute("active");
+        if (acc == null) {
+            mv = new ModelAndView("redirect:/auth");
+            ra.addFlashAttribute("upperNotification", "Please log in again (Session expired)!");
+            return mv;
+
+        }
+        itService.reportIssueWithData(params);
+        ra.addFlashAttribute("upperNotification", "Issue reported!");
+        return mv;
+    }
+
+
 
     @GetMapping("/createAccount")
     public String createAccount(@ModelAttribute("emplId") int id_empl, @ModelAttribute("tlId") int id_tl, RedirectAttributes ra) {
@@ -31,4 +66,6 @@ public class ITController {
         return "redirect:/";
 
     }
+
+
 }
