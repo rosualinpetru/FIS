8e461772729397f61b087af918ccbb45fa695e1b
Rosu Alin-Petru <rosualinpetru@gmail.com>
2020-06-19 20:56:52 +0300

    TOM-55 #comment IssueRequestService Tested

--- src/main/java/ro/go/redhomeserver/tom/dtos/PendingIssue.java
+++ src/main/java/ro/go/redhomeserver/tom/dtos/PendingIssue.java
@@ -2,12 +2,14 @@ package ro.go.redhomeserver.tom.dtos;
 
 
 import lombok.AllArgsConstructor;
+import lombok.EqualsAndHashCode;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@EqualsAndHashCode
 public class PendingIssue {
     private String id;
     private String departmentName;
