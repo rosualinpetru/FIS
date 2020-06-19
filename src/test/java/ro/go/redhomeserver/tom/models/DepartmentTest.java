f1ef7e9370d0f9740ea6ee8fc8f0f9a075ddc87f
Rasinar Ioan <rasinarionut@yahoo.com>
2020-06-19 13:57:06 +0300

    TOM-55 #comment Tested Department Model  !

---
+++ src/test/java/ro/go/redhomeserver/tom/models/DepartmentTest.java
@@ -0,0 +1,36 @@
+package ro.go.redhomeserver.tom.models;
+
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.*;
+
+public class DepartmentTest {
+    @Test
+    void checkConstructorAndGetters() {
+        Department department = new Department("It");
+        assertThat(department.getName().equals("It")).isTrue();
+        assertThat(department.getId()).isNull();
+    }
+
+    @Test
+    void checkNoArgsConstructor() {
+        Department department = new Department();
+        assertThat(department.getName()).isNull();
+        assertThat(department.getId()).isNull();
+    }
+
+    @Test
+    void checkSetter() {
+        Department department = new Department();
+        department.setName("It");
+        assertThat(department.getName().equals("It")).isTrue();
+    }
+
+    @Test
+    void checkEquals() {
+        Department department1 = new Department("It");
+        Department department2 = new Department("It");
+        assertThat(department1.equals(department2)).isTrue();
+    }
+}
